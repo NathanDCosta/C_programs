536833887f0e96d354f487109d99b7f54256a57f
NathanDCosta <nathandcosta22@gmail.com>
2025-10-15 11:39:30 +0530

    Create matrix_operation.c

---
+++ matrix_operation.c
@@ -0,0 +1,15 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int i,j,r1,c1,r2,c2;
+   int mat1[5][5];mat2[5][5];
+   printf("enter number of rows and cols of matrix1");
+   scanf("%d  ");
+    for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
+        
+    }
+
+    return 0;
+}
