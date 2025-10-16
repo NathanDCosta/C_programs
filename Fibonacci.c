22104445a17a0340a039180cea706b19936a10b0
NathanDCosta <nathandcosta22@gmail.com>
2025-10-16 10:47:13 +0530

    Create Fibonacci.c

---
+++ Fibonacci.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+	int n1=0,n2=1,next,num,i=0;
+ printf("enter number of elements");
+ scanf("%d",&num);
+ for(i=0;i<num;i++){
+ 	printf("%d",n1);
+ 	next=n1+n2;
+ 	n1=n2;
+ 	n2=next;
+ }
+}
