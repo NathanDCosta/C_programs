916532086d022a1de05cecb764273ec64b760f30
NathanDCosta <nathandcosta22@gmail.com>
2025-10-17 22:50:55 +0530

    Update banking.c
    Updated transfer function to check for sufficient funds before transferring. Improved user prompts for clarity.

--- functions/banking.c
+++ functions/banking.c
@@ -1,29 +1,40 @@
 #include<stdio.h>
 
 int interest(int p,int r,int t){
-
 	int interest = (p*r*t)/100;
 	return interest;
 }
+
 int transfer(int *acc1,int *acc2,int funds){
-    *acc1=*acc1-funds;
+  
+	if(*acc1<funds){
+	    
+		printf("insuffiecent funds");
+	}
+	else{
+	    *acc1=*acc1-funds;
     *acc2=*acc2 +funds;
-    printf("Balance in account 1  after transfer is : %d \n Balance in account 2  after transfer is %d",*acc1,*acc2);
+    printf("Balance in account 1  after transfer is : %d \nBalance in account 2  after transfer is %d",*acc1,*acc2);
+	}
 }
 
 int main(){
-	  int balance1,principal,rate=7,time,intbal,balance2,funds;
+    printf("\t\t ONLINE BANKING\n\n");
+	  int balance1,principal,rate,time,intbal,balance2,funds;
 	  printf("Enter account balance : ");
     scanf("%d",&balance1);
 	  printf("Enter principal amount : ");
     scanf("%d",&principal);
+    printf("Enter rate of interest : ");
+    scanf("%d",&rate);
+    
 	
-	  printf("Enter time in years : ");
+	  printf("Enter time period (in years) : ");
     scanf("%d",&time);
 
     int inter =interest(principal,rate,time);
     intbal= inter +balance1;
-    printf("\n \n Current rate of interest is %d \n Interest earned is %d \n Account balance is %d \n\n",rate,inter,intbal);
+    printf("\n \nInterest earned is %d \nAccount balance is %d \n\n",inter,intbal);
     printf("Enter account 2 balance : ");
     scanf("%d",&balance2);
     printf("Enter funds to transfer : ");
