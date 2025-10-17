f84140578b84d8723e0c1b8e8cf88f7b02171790
NathanDCosta <nathandcosta22@gmail.com>
2025-10-17 09:37:52 +0000

    changes

--- functions/statistics.c
+++ functions/statistics.c
@@ -1,1 +1,21 @@
-#Include
+#include<stdio.h>
+int mean(int arr[],int n){
+    int mean=0;
+    for(int i=0;i<n;i++){
+        mean +=arr[i];
+    }
+    printf("%d",mean);
+
+}
+ int main(){
+    int n,arr[1];
+    printf("enter elemnts of array");
+    scanf("%d,&n");
+    arr[n];
+     for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    mean(arr[n],n);
+
+
+ }
