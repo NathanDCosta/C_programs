98c23f0e6f61774e594a7eda53a120ef91213cb7
NathanDCosta <nathandcosta22@gmail.com>
2025-10-17 21:18:59 +0530

    Fix mean calculation and implement mode function
    Updated mean function to calculate average correctly and added mode function to find the mode of the array.

--- functions/statistics.c
+++ functions/statistics.c
@@ -1,21 +1,51 @@
 #include<stdio.h>
 int mean(int arr[],int n){
-    int mean=0;
+    float mean=0;
     for(int i=0;i<n;i++){
         mean +=arr[i];
     }
-    printf("%d",mean);
+    mean = mean/2;
+    printf("%.3f",mean);
 
 }
+int mode(int arr[],int n){
+    int mode=arr[0];
+    int count =1,maxcount=1;
+    
+    for(int i=0;i<n;i++){
+        count =1;
+        for(int j=i+1;j<n;j++){
+            if(arr[j]==arr[i]){
+                count++;
+            }
+            
+            
+        }
+        if(count>maxcount){
+            maxcount=count;
+            mode=arr[i];
+        }
+    }
+    if(maxcount==1){
+   
+          printf("no mode");
+    }
+    else{
+      
+         printf("mode is %d",mode);
+    }
+    
+}
  int main(){
-    int n,arr[1];
-    printf("enter elemnts of array");
-    scanf("%d,&n");
-    arr[n];
+    int n;
+    printf("enter number of elemnts in array");
+    scanf("%d",&n);
+    int arr[n];
      for(int i=0;i<n;i++){
         scanf("%d",&arr[i]);
     }
-    mean(arr[n],n);
+    mean(arr,n);
+    mode(arr,n);
 
 
  }
