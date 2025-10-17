3f58e711dae4539e078beebc9c09202f7d6c14fa
NathanDCosta <nathandcosta22@gmail.com>
2025-10-17 13:39:55 +0530

    Implement swap functions for integers
    using call by reference and call by value

---
+++ functions/Swapping.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<conio.h>
+int swap(int x,int y);
+int swap2(int *x,int *y);
+
+int swap(int x,int y){
+
+	int temp = x;
+	x=y;
+	y=temp;
+	printf(" x=%d ,y=%d \n",x,y);
+}
+int swap2(int *x,int *y){
+
+	int temp = *x;
+	*x = *y;
+	*y=temp;
+	printf("x= %d ,y=%d \n",*x,*y);
+}
+
+
+
+int main(){
+	int a,b,c,d;
+	printf("enter 2 numbers to swap using call by value \n");
+	scanf("%d %d",&a,&b);
+	swap(a,b);
+	printf("enter 2 numbers to swap using call by value \n");
+	scanf("%d %d",&c,&d);
+	swap2(&c,&d);
+	getch();
+	return 0;
+}
