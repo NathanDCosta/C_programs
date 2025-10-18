c4ae4c492e578abeb4823e68b5e2c44e8559d8c1
NathanDCosta <nathandcosta22@gmail.com>
2025-10-18 09:27:43 +0530

    changes

--- matrix/matrix_operation.c
+++ matrix/matrix_operation.c
@@ -2,14 +2,32 @@
 #include <stdio.h>
 
 int main() {
-    int i,j,r1,c1,r2,c2;
-   int mat1[5][5];mat2[5][5];
-   printf("enter number of rows and cols of matrix1");
-   scanf("%d  ");
-    for(i=0;i<n;i++){
-        scanf("%d",&a[i]);
-        
+    int r1,c1,r2,c2;
+ 
+
+    printf("enter number of rows and cols of matrix1 : ");
+    scanf("%d  %d",&r1,&c1);
+    printf("\nenter number of rows and cols of matrix2 : ");
+     scanf("%d  %d",&r2,&c2);
+        int mat1[r1][c1],mat2[r2][c2];
+
+        printf("\nenter elements of matrix 1");
+        for(int i=0;i<r1;i++){
+            for ( int j = 0; j < c1; j++)
+            {
+                scanf("%d",&mat1[i][j]);
+            }
+        }
+
+    
+    printf("\nenter elements of matrix 2");
+    for(int i=0;i<r2;i++){
+        for ( int j = 0; j < c2; j++)
+        {
+              scanf("%d",&mat2[i][j]);
+        }
     }
+
 
     return 0;
 }
